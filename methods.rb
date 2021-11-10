3d440371b3dc05295718f102603243895817d1c6
tar-alex994 <alextar994@gmail.com>
2021-11-10 10:14:57 +0300

    добавлен метод get_descreption

--- methods.rb
+++ methods.rb
@@ -10,3 +10,20 @@ def get_name
   name = name.scan(/[A-ZА-ЯЁ]+/i).map(&:capitalize).join(' ')
   # строка очищается от лишних пробелов, и форматируется регистр
 end
+
+
+def get_descreption
+  begin
+    puts "Опишите деятельность человека(не более 60 символов):"
+
+    descreption = STDIN.gets.chomp
+  end while descreption.size > 60 && descreption =~ /<.+>/
+  # regexp проверяет наличие вредоносного кода в описании
+  
+  descreption.strip!
+  descreption[0] = descreption[0].upcase
+  # описание всегда будет начинаться с большой буквы и не будет содержать
+  # пробелов в начале и в конце строки
+
+  descreption
+end
