712581d083790755369b33f8e896b3743941de1b
tar-alex994 <alextar994@gmail.com>
2021-11-10 15:05:11 +0300

    добавлен метод create_html

--- personal_card_class.rb
+++ personal_card_class.rb
@@ -73,4 +73,18 @@ class PersonalCard
 
     "Email: #{email}"
   end
+
+
+  def create_html
+    folder_path = File.dirname(__FILE__)
+    file_path   = folder_path + "/personal_cards/pCard_#{@name}.html"
+    file        = File.new(file_path, 'w:UTF-8')
+
+    img_tag = "  <img src=\"#{@image}\" alt=\"Личное фото\">\n"
+    p_tags  = [@name, @descreption, @phone, @email]
+    p_tags  = p_tags.map { |el| "  <p>#{el}</p>\n" }.join
+
+    file.puts("<html>\n<body>\n#{img_tag + p_tags}</body>\n</html>")
+    file.close
+  end
 end
